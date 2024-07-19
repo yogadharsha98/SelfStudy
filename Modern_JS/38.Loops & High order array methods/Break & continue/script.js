da3277ac90401b0bd4b5fbe5e39e8dbad7432caf
yogakrishanthi98 <yogakrishanthi@konnect.team>
2024-07-19 07:54:31 +0530

    Break & Continue

---
+++ Modern_JS/38.Loops & High order array methods/Break & continue/script.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 3) {
+//     console.log('Breaking....');
+//     break;
+//   }
+//   console.log(i);
+// }
+
+for (let i = 0; i <= 10; i++) {
+  if (i === 5) {
+    console.log('Skipping 5....');
+    continue;
+  }
+  console.log(i);
+}
