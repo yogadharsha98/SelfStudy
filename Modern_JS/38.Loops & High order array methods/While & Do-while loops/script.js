127a4b078b2e33e8574c5630dfbd0d3789eec555
yogakrishanthi98 <yogakrishanthi@konnect.team>
2024-07-19 08:05:11 +0530

    While & Do-while loops

---
+++ Modern_JS/38.Loops & High order array methods/While & Do-while loops/script.js
@@ -0,0 +1,31 @@
+let i = 21;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+//loop over array
+// const arr = [10, 20, 30, 40];
+
+// while (i < arr.length) {
+//   console.log(arr[i]);
+//   i++;
+// }
+
+//nested while loop
+
+// while (i <= 5) {
+//   console.log(i);
+//   let j = 1;
+//   while (j <= 5) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//     j++;
+//   }
+//   i++;
+// }
+
+do {
+  console.log('Number' + i);
+  i++;
+} while (i <= 20);
