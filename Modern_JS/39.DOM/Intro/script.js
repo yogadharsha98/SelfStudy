dccc4025f5508e0a3267031b78a721c0331f90e7
yogakrishanthi98 <yogakrishanthi@konnect.team>
2024-07-23 07:30:53 +0530

    DOM intro

---
+++ Modern_JS/39.DOM/Intro/script.js
@@ -0,0 +1,15 @@
+// console.dir(window.document);
+
+// console.log(document.body.innerHTML);
+
+// console.log(document.links[0]);
+
+// document.body.innerHTML = '<h1>Hey there</h1>';
+
+// document.write('Hello js');
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+// main.innerHTML = '<h1>Hey main</h1>';
+
+document.querySelector('#main li').textContent = 'Hello';
