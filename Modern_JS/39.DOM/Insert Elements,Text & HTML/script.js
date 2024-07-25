55cc7fbcf5e259168cf7a1a1fa11b5bb000f5657
yogakrishanthi98 <yogakrishanthi@konnect.team>
2024-07-25 11:20:51 +0530

    Insert elements, Text &HTML

---
+++ Modern_JS/39.DOM/Insert Elements,Text & HTML/script.js
@@ -0,0 +1,40 @@
+//insertAdjacentElement example
+
+function insertElement() {
+  const filter = document.querySelector('.filter');
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'insertAdjacentElement';
+
+  filter.insertAdjacentElement('beforebegin', h1);
+}
+
+//insertAdjacentText example
+function insertText() {
+  const item = document.querySelector('li:first-child');
+
+  item.insertAdjacentText('beforebegin', 'insertAdjacentText');
+}
+//insertAdjacentHTML example
+function insertHTML() {
+  const clearBtn = document.querySelector('#clear');
+
+  clearBtn.insertAdjacentHTML('afterend', '<h2>insertAdjacentHTML</h2>');
+}
+
+//insertBefore example
+function insertBeforeItem() {
+  const ul = document.querySelector('ul');
+
+  const li = document.createElement('li');
+
+  li.textContent = 'insertBefore';
+
+  const thirdItem = document.querySelector('li:nth-child(3)');
+  ul.insertBefore(li, thirdItem);
+}
+
+insertElement();
+insertText();
+insertHTML();
+insertBeforeItem();
