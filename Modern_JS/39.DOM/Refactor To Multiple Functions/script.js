0d8476192444141d06063534c95965e8a1d3f9ff
yogakrishanthi98 <yogakrishanthi@konnect.team>
2024-07-25 10:15:39 +0530

    Refactor to Multiple functions

---
+++ Modern_JS/39.DOM/Refactor To Multiple Functions/script.js
@@ -0,0 +1,31 @@
+function createNewItem(item) {
+  const li = document.createElement('li');
+  const itemText = document.createTextNode(item);
+
+  li.appendChild(itemText);
+
+  const button = createButton('remove-item btn-link text-red');
+
+  li.appendChild(button);
+
+  document.querySelector('.items').appendChild(li);
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+
+  return icon;
+}
+
+createNewItem('cheese');
+createNewItem('Sauce');
